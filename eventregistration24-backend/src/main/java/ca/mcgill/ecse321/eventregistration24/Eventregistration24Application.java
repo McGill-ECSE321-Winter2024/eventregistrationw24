96f8b06b45046dfaf8929fde6091d2b5e8b91af6
Anthony Ubah <anthony.ubah@mail.mcgill.ca>
2024-01-25 15:20:43 -0500

    Initial commit

---
+++ eventregistration24-backend/src/main/java/ca/mcgill/ecse321/eventregistration24/Eventregistration24Application.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventregistration24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eventregistration24Application.class, args);
+	}
+
+}
---
+++ eventregistration24-backend/src/test/java/ca/mcgill/ecse321/eventregistration24/Eventregistration24ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventregistration24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
